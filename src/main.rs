56689d779b90f2901309d6ed6510ae5e48f17a73
toshi_pp <toshiq2@gmail.com>
2017-06-25 07:15:48 +0900

    support multi threads

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use std::ptr;
 use std::slice;
 use std::thread;
 use std::sync::atomic::AtomicPtr;
+use std::env;
 
 const NBD_SET_SOCK: u64 = 43776;
 const NBD_SET_BLKSIZE: u64 = 43777;
@@ -26,6 +27,8 @@ const NBD_SET_FLAGS: u64 = 43786;
 const NBD_REQUEST_MAGIC: u32 = 0x25609513;
 const NBD_REPLY_MAGIC: [u8; 4] = [0x67, 0x44, 0x66, 0x98];
 
+const NBD_FLAG_CAN_MULTI_CONN: u64 = (1 << 8);
+
 const NBD_CMD_READ: u32 = 0;
 const NBD_CMD_WRITE: u32 = 1;
 const NBD_CMD_DISC: u32 = 2;
@@ -34,13 +37,6 @@ const NBD_CMD_TRIM: u32 = 4;
 
 const BLOCK_SIZE: u64 = 4096;
 const DEVICE_SIZE: u64 = 3 * 1024 * 1024 * 1024 / 2;
-
-struct NBD {
-    nbd: File,
-    _client_sock: UnixStream,
-    server_sock: UnixStream,
-    mem: Vec<u8>,
-}
 
 #[derive(Debug)]
 #[repr(C)]
@@ -59,35 +55,20 @@ struct nbd_reply {
     error: u32,
     handle: [u8; 8],
 }
-*/
+ */
 
-impl NBD {
-    fn init() -> Result<NBD> {
-        let f = File::open("/dev/nbd0")?;
-        unsafe {
-            let fd = f.as_raw_fd();
-            if libc::ioctl(fd, NBD_CLEAR_SOCK) == -1 {
-                return Err(Error::last_os_error());
-            }
-            if libc::ioctl(fd, NBD_SET_BLKSIZE, BLOCK_SIZE) == -1 ||
-                libc::ioctl(fd, NBD_SET_SIZE_BLOCKS, DEVICE_SIZE / BLOCK_SIZE) == -1
-            {
-                return Err(Error::last_os_error());
-            }
-            let (c, s) = UnixStream::pair()?;
-            if libc::ioctl(fd, NBD_SET_SOCK, c.as_raw_fd() as u64) == -1 {
-                return Err(Error::last_os_error());
+struct Handler {
+    sock: UnixStream,
+    p: *mut u8,
+    l: usize,
+}
 
-            }
-
-            let mut v = Vec::with_capacity(DEVICE_SIZE as usize);
-            v.set_len(DEVICE_SIZE as usize);
-            Ok(NBD {
-                nbd: f,
-                _client_sock: c,
-                server_sock: s,
-                mem: v,
-            })
+impl Handler {
+    fn new<'a>(sock: UnixStream, mem: &'a mut [u8]) -> Handler {
+        Handler {
+            sock: sock,
+            p: mem.as_mut_ptr(),
+            l: mem.len(),
         }
     }
 
@@ -96,7 +77,7 @@ impl NBD {
             let mut req = uninitialized::<nbd_request>();
             let p: *mut u8 = transmute(&mut req);
             let buf = slice::from_raw_parts_mut(p, size_of::<nbd_request>());
-            self.server_sock.read_exact(buf).unwrap();
+            self.sock.read_exact(buf).unwrap();
             req.magic = u32::from_be(req.magic);
             req.type_ = u32::from_be(req.type_);
             req.from = u64::from_be(req.from);
@@ -110,23 +91,25 @@ impl NBD {
         let begin = req.from as usize;
         let end = begin + req.len as usize;
         self.send_header(req)?;
-        self.server_sock.write_all(&self.mem[begin..end])?;
+        let s = unsafe { slice::from_raw_parts(self.p, self.l) };
+        self.sock.write_all(&s[begin..end])?;
         Ok(())
     }
 
     fn process_write(&mut self, req: nbd_request) -> Result<()> {
         let begin = req.from as usize;
         let end = begin + req.len as usize;
-        self.server_sock.read_exact(&mut self.mem[begin..end])?;
+        let s = unsafe { slice::from_raw_parts_mut(self.p, self.l) };
+        self.sock.read_exact(&mut s[begin..end])?;
         self.send_header(req)?;
         Ok(())
     }
 
     fn send_header(&mut self, req: nbd_request) -> Result<()> {
-        self.server_sock.write_all(&NBD_REPLY_MAGIC)?;
+        self.sock.write_all(&NBD_REPLY_MAGIC)?;
         let error = [0u8; 4];
-        self.server_sock.write_all(&error)?;
-        self.server_sock.write_all(&req.handle)?;
+        self.sock.write_all(&error)?;
+        self.sock.write_all(&req.handle)?;
         Ok(())
     }
 
@@ -146,28 +129,89 @@ impl NBD {
     fn start_thread(&mut self) -> thread::JoinHandle<()> {
         let p = AtomicPtr::new(self);
         thread::spawn(move || {
-            let mut nbd = unsafe { p.into_inner().as_mut().unwrap() };
+            let mut h = unsafe { p.into_inner().as_mut().unwrap() };
             loop {
-                let req = nbd.recv_req().unwrap();
-                if !nbd.process_req(req).unwrap() {
+                let req = h.recv_req().unwrap();
+                if !h.process_req(req).unwrap() {
                     break;
                 }
             }
-            nbd.server_sock.shutdown(net::Shutdown::Both).unwrap();
+            h.sock.shutdown(net::Shutdown::Both).unwrap();
         })
+    }
+}
+
+struct NBD {
+    nbd: File,
+    _client_socks: Vec<UnixStream>,
+    handlers: Vec<Handler>,
+    _mem: Vec<u8>,
+}
+
+impl NBD {
+    fn init(n: usize) -> Result<NBD> {
+        let f = File::open("/dev/nbd0")?;
+        let fd = f.as_raw_fd();
+        let mut cv = Vec::new();
+        let mut hv = Vec::new();
+        unsafe {
+            let mut mem = Vec::with_capacity(DEVICE_SIZE as usize);
+            mem.set_len(DEVICE_SIZE as usize);
+
+            if libc::ioctl(fd, NBD_CLEAR_SOCK) == -1 {
+                return Err(Error::last_os_error());
+            }
+            if libc::ioctl(fd, NBD_SET_FLAGS, NBD_FLAG_CAN_MULTI_CONN) == -1 {
+                return Err(Error::last_os_error());
+            }
+            if libc::ioctl(fd, NBD_SET_BLKSIZE, BLOCK_SIZE) == -1 ||
+                libc::ioctl(fd, NBD_SET_SIZE_BLOCKS, DEVICE_SIZE / BLOCK_SIZE) == -1
+            {
+                return Err(Error::last_os_error());
+            }
+
+            for _ in 0..n {
+                let (c, s) = UnixStream::pair()?;
+                if libc::ioctl(fd, NBD_SET_SOCK, c.as_raw_fd() as u64) == -1 {
+                    return Err(Error::last_os_error());
+
+                }
+                cv.push(c);
+                hv.push(Handler::new(s, &mut mem[..]));
+            }
+
+
+            Ok(NBD {
+                nbd: f,
+                _client_socks: cv,
+                handlers: hv,
+                _mem: mem,
+            })
+        }
     }
 
     fn run(&mut self) -> Result<()> {
-        let h = self.start_thread();
+        let jhs: Vec<_> = self.handlers.iter_mut().map(|h| h.start_thread()).collect();
+
         let fd = self.nbd.as_raw_fd();
         unsafe {
             if libc::ioctl(fd, NBD_DO_IT) == -1 {
                 return Err(Error::last_os_error());
             }
         }
-        h.join().map_err(
-            |e| Error::new(ErrorKind::Other, format!("{:?}", e)),
-        )
+
+        let mut ret = Ok(());
+        for r in jhs.into_iter().map(|jh| {
+            jh.join().map_err(|e| {
+                Error::new(ErrorKind::Other, format!("{:?}", e))
+            })
+        })
+        {
+            if r.is_err() {
+                ret = r;
+            }
+        }
+        ret
     }
 
     fn stop(&mut self) -> Result<()> {
@@ -204,7 +248,8 @@ fn handle_signal(nbd: &mut NBD) -> Result<thread::JoinHandle<()>> {
 }
 
 fn main() {
-    let mut nbd = NBD::init().unwrap();
+    let n: usize = env::args().nth(1).expect("# of threads").parse().unwrap();
+    let mut nbd = NBD::init(n).unwrap();
     let h = handle_signal(&mut nbd).unwrap();
     print!("start\n");
     nbd.run().unwrap();
